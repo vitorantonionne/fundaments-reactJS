05046a91b4c57d61a52fcd6ce5bf10c145cec389
Vitor Antonionne <vitor-antonionne@hotmail.com>
2024-04-24 19:52:40 -0300

    finish header , post , sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import './App.css'
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
-function App() {
+import styles from './css/App.module.css'
+import './global.css'
+
+export function App() {
   return (
-    
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post 
+            author="Vitor Antonionne"
+            content="Lorem ipsum dolor sit amet consectetur adipisic"
+          />
+          <Post 
+            author="Vitor Antonionne"
+            content="Lorem ipsum dolor sit amet consectetur adipisic"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
-export default App
